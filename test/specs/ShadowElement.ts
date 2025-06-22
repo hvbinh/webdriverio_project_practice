ff831ed9df7703855c469ae5e16a25885d5d0ac7
hvbinh <havanbinhk13t01@gmail.com>
2025-06-22 12:55:53 +0700

    update remove some comment

--- test/specs/ShadowElement.ts
+++ test/specs/ShadowElement.ts
@@ -1,9 +1,9 @@
 
-// describe('Shadow example',function()
-// {
-//     it('Verify can click on shadow element',async ()=>{
-//         await browser.url('https://shop.polymer-project.org/')
-//         const shadowHost = await $("shop-app[page='home']")  //element chứa shadowroot
-//         await shadowHost.shadow$("a[href='/list/mens_outerwear']").click()  //click on shadow element test1_branch testing
-//     })
-// }) 
+describe('Shadow example',function()
+{
+    it('Verify can click on shadow element',async ()=>{
+        await browser.url('https://shop.polymer-project.org/')
+        const shadowHost = await $("shop-app[page='home']")  //element chứa shadowroot
+        await shadowHost.shadow$("a[href='/list/mens_outerwear']").click()  //click on shadow element test1_branch testing
+    })
+}) 
